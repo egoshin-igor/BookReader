d5f9f9346ff596a9450da836b0c87b4f19ab5100
igor.egoshin <igor.egoshin@mail.ru>
2019-10-12 17:04:43 +0300

    Изменил даты в книге пользователя

--- BookReader.Api/Migrations/20191012140251_MakeUserBookDatesNullable.Designer.cs
+++ BookReader.Api/Migrations/20191012140251_MakeUserBookDatesNullable.Designer.cs
@@ -4,14 +4,16 @@ using BookReader.Infrastructure.Foundation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookReader.Api.Migrations
 {
     [DbContext(typeof(BookReaderDbContext))]
-    partial class BookReaderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191012140251_MakeUserBookDatesNullable")]
+    partial class MakeUserBookDatesNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BookReader.Core/Model/UserBook.cs
+++ BookReader.Core/Model/UserBook.cs
@@ -7,8 +7,8 @@ namespace BookReader.Core.Model
         public int Id { get; protected set; }
         public int BookId { get; protected set; }
         public int UserId { get; protected set; }
-        public DateTime StartReadingOnUtc { get; protected set; }
-        public DateTime FinisReadingOnUtc { get; protected set; }
+        public DateTime? StartReadingOnUtc { get; protected set; }
+        public DateTime? FinisReadingOnUtc { get; protected set; }
         public UserBookStatus Status { get; protected set; }
         public string Note { get; protected set; }
 
